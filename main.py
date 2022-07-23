acb1c94e4e19e8b78d0fb03f75e6b990cec2ae11
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2022-07-23 22:06:33 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from utils import add
+
+
+def main():
+    result_1 = add(1, 2)
+    print(result_1)
+
+
+if __name__ == '__main__':
+    main()
+    
---
+++ utils.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    """
+    :param a: first number (any type)
+    :param b: second number (any type)
+    :return: sum of two number
+    """
+    return a + b
