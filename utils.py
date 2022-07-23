e378d1f6b775bcba32dc8170fc7472bb89f28854
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2022-07-23 22:24:15 +0700

    Multiplication function added.

--- utils.py
+++ utils.py
@@ -17,3 +17,12 @@ def subtract(first_number, second_number):
     :return: difference between two numbers
     """
     return first_number - second_number
+
+
+def multiply(first_number, second_number):
+    """
+    :param first_number: first number (any type)
+    :param second_number: second number (any type)
+    :return: multiplication of two numbers
+    """
+    return first_number * second_number
