b8f07d2a5223566815e539bad145bb1b6e2e7227
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2022-08-01 22:35:13 +0700

    test_add() added (temporarily off).

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,19 +1,19 @@
 """Testing of utils"""
 
-from src.calculator.utils import add
-
-
-def test_add():
-    """
-    Test add function
-    :return:
-    """
-    assert add(1, 2) is 3
-    assert add(3, 4) is 7
-    assert add(0, 2) is 2
-    assert add(0, 0) is 0
-    assert add(0, -1) is -1
-    assert add(-1, -2) is -3
-    assert add(1, -1) is 0
-    assert add(3, -4) is -1
-    assert add(5, -3) is 2
+# from src.calculator.utils import add
+#
+#
+# def test_add():
+#     """
+#     Test add function
+#     :return:
+#     """
+#     assert add(1, 2) is 3
+#     assert add(3, 4) is 7
+#     assert add(0, 2) is 2
+#     assert add(0, 0) is 0
+#     assert add(0, -1) is -1
+#     assert add(-1, -2) is -3
+#     assert add(1, -1) is 0
+#     assert add(3, -4) is -1
+#     assert add(5, -3) is 2
