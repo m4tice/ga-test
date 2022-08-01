ab9f1aa466ed1721b539c9b26fedfc51acfa872a
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2022-08-01 22:19:18 +0700

    test_add() added.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,4 +8,12 @@ def test_add():
     Test add function
     :return:
     """
-    assert add(1, 2), 3
+    assert add(1, 2) is 3
+    assert add(3, 4) is 7
+    assert add(0, 2) is 2
+    assert add(0, 0) is 0
+    assert add(0, -1) is -1
+    assert add(-1, -2) is -3
+    assert add(1, -1) is 0
+    assert add(3, -4) is -1
+    assert add(5, -3) is 2
